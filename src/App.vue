02935be5c330635a24d38816b7aa20bd64b562de
Nick Hernandez <xnickhernandez@gmail.com>
2025-04-27 20:48:51 -0700

    Add filters

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,45 @@ import { PlusCircleIcon, XCircleIcon, TrashIcon } from '@heroicons/vue/24/solid'
     <h1 class="text-2xl text-center mb-4">TaskFlow</h1>
   </header>
   <main>
-    <div id="todo-board__container" class="text-center">
-      <form @submit.prevent="addTask" class="task-form flex flex-col">
-        <label for="new-task">Task:</label>
-        <input v-model="newTask" name="new-task" type="text" class="border border-black" />
-        <div class="task-form__action-buttons flex mx-auto">
-          <XCircleIcon
-            @click="clearTask"
-            class="w-8 text-gray-500 hover:text-gray-700 transition duration-200"
-          />
-          <button type="submit">
-            <PlusCircleIcon class="w-8 text-blue-500 hover:text-blue-600 duration-200" />
-          </button>
-        </div>
-      </form>
+    <form @submit.prevent="addTask" class="task-form task-form flex flex-col">
+      <label for="new-task">Task:</label>
+      <input v-model="newTask" name="new-task" type="text" class="border border-black" />
+      <div class="task-form__action-buttons flex mx-auto">
+        <XCircleIcon
+          @click="clearTask"
+          class="w-8 text-gray-500 hover:text-gray-700 transition duration-200"
+        />
+        <button type="submit">
+          <PlusCircleIcon class="w-8 text-blue-500 hover:text-blue-600 duration-200" />
+        </button>
+      </div>
+    </form>
+
+    <div class="filters flex justify-start gap-4 mt-6">
+      <button
+        :class="['filters__button', activeFilter === 'all' ? 'active-filter' : '']"
+        @click="setFilter('all')"
+      >
+        All
+      </button>
+      <button
+        :class="['filters__button', activeFilter === 'active' ? 'active-filter' : '']"
+        @click="setFilter('active')"
+      >
+        Active
+      </button>
+      <button
+        :class="['filters__button', activeFilter === 'completed' ? 'active-filter' : '']"
+        @click="setFilter('completed')"
+      >
+        Completed
+      </button>
     </div>
-    <div class="tasks__container">
-      <ul v-if="tasks[0]" class="tasks__list">
+
+    <div class="tasks-list">
+      <ul v-if="tasks[0]" class="tasks-list__list">
         <li
-          v-for="task in tasks"
+          v-for="task in filteredTasks"
           :key="task.id"
           class="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition mb-4 flex justify-between items-center"
         >
@@ -35,7 +55,16 @@ import { PlusCircleIcon, XCircleIcon, TrashIcon } from '@heroicons/vue/24/solid'
             @change="updateTaskStatus(task.id)"
             class="mr-2"
           />
-          <p class="mr-auto inline">{{ task.text }}</p>
+          <span
+            :class="[
+              'mr-auto',
+              task.completed
+                ? 'line-through text-gray-400 opacity-70 transition-colors duration-200'
+                : 'text-gray-800 transition-colors duration-200',
+            ]"
+          >
+            <p>{{ task.text }}</p>
+          </span>
           <TrashIcon
             @click="deleteTask(task.id)"
             class="w-8 text-gray-500 hover:text-gray-700 transition duration-200"
@@ -55,7 +84,19 @@ export default {
     return {
       newTask: '',
       tasks: [],
+      activeFilter: 'All',
     }
+  },
+  computed: {
+    filteredTasks() {
+      if (this.activeFilter === 'active') {
+        return this.tasks.filter((task) => !task.completed)
+      } else if (this.activeFilter === 'completed') {
+        return this.tasks.filter((task) => task.completed)
+      } else {
+        return this.tasks
+      }
+    },
   },
   methods: {
     addTask() {
@@ -81,15 +122,24 @@ export default {
     },
     deleteTask(id) {
       const selectedTaskIndex = this.tasks.findIndex((task) => task.id === id)
-      console.log('Selected Task Index: ', selectedTaskIndex)
       if (selectedTaskIndex !== -1) {
         this.tasks.splice(selectedTaskIndex, 1)
       }
-      console.log('this.tasks: ', this.tasks)
-
+    },
+    setFilter(option) {
+      console.log('option: ', option)
+      if (this.activeFilter === option) return
+      this.activeFilter = option
     },
   },
 }
 </script>
 
-<style></style>
+<style>
+.filters__button {
+  @apply px-4 py-2 border border-gray-300 rounded-md text-gray-600 hover:text-blue-500 hover:bg-gray-100 transition-colors duration-200 cursor-pointer font-medium;
+}
+.active-filter {
+  @apply text-blue-600 bg-blue-100 border-blue-400 font-semibold;
+}
+</style>
